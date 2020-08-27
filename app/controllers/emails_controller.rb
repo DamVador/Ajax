5a488f57275b4d7ef05029729f810bb8592d8c97
Damien <damienbanville@hotmail.fr>
2020-08-27 16:01:17 +0200

    some issues fixed and not working ajax readed mails

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -6,7 +6,8 @@ class EmailsController < ApplicationController
 
   def create
     @email=Email.create(object: Faker::Lorem.sentence,
-      body: Faker::Lorem.paragraphs)
+      body: Faker::Lorem.paragraphs,
+    read: false)
 
       respond_to do |format|
         format.html { redirect_to root_path }
@@ -16,11 +17,20 @@ class EmailsController < ApplicationController
 
   def show
     @email=Email.find(params[:id])
-
+    @email.update(read: true)
       respond_to do |format|
         format.html { redirect_to root_path }
         format.js { }
       end
+  end
+
+  def update
+    @email=Email.find(params[:id])
+    @email.update(:read => params[:read])
+    respond_to do |format|
+      format.html { redirect_to emails_path }
+      format.js { }
+    end
   end
 
   def destroy
